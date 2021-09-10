e592d6c7b51c40bc34c1be639091d8b593af3373
Andy D. Ng <itc.anhduy@gmail.com>
2021-09-10 11:01:37 +0700

    feat: supported multi requests

--- build/types/request.d.ts
+++ build/types/request.d.ts
@@ -1,24 +1,29 @@
-import { AxiosError } from 'axios';
-import { TDataRequest, TNormalRequest, TRequest } from './base';
-import { TStateValues } from './state';
+import { AxiosError } from 'axios'
+import {
+  TDataRequest,
+  TNormalRequest,
+  TRequest,
+  TRequestResponse
+} from './base'
+import { TStateValues } from './state'
 export declare type TRequestMachineEvent<T> = {
-    type: 'REQUEST';
-    request: TRequest<T>;
-};
+  type: 'REQUEST'
+  request: TRequest<T>
+}
 export declare type TRequestMachineState<T> = {
-    value: TStateValues;
-    context: IRequestMachineContext<T>;
-};
+  value: TStateValues
+  context: IRequestMachineContext<T>
+}
 export interface IRequestMachineContext<T> {
-    data: T;
-    error: AxiosError;
+  data: TRequestResponse<T>
+  error: AxiosError
 }
 export interface ICreateRequests {
-    onDelete: TNormalRequest;
-    onGet: TNormalRequest;
-    onOptions: TNormalRequest;
-    onHead: TNormalRequest;
-    onPatch: TDataRequest;
-    onPost: TDataRequest;
-    onPut: TDataRequest;
+  onDelete: TNormalRequest
+  onGet: TNormalRequest
+  onOptions: TNormalRequest
+  onHead: TNormalRequest
+  onPatch: TDataRequest
+  onPost: TDataRequest
+  onPut: TDataRequest
 }
