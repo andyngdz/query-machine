743cca9047550280ea3d788df195ab5852e92b32
Andy D. Ng <itc.anhduy@gmail.com>
2021-09-16 16:35:30 +0700

    feat: exported types

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,3 +1,4 @@
-export * from './hooks';
-export * from './services';
-export * from './states';
+export * from './hooks'
+export * from './services'
+export * from './states'
+export * from './types'
